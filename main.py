62459c3ef7703bf3a2b60d61447d7de40a3984df
Daniel Lage <80716732+Daniel-Lage@users.noreply.github.com>
2022-01-19 11:52:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 file_name=input()
-
-with open(f"schedule/{file_name}.py", "w") as f:
-  f.write("import os; os.remove(__file__)")
+try:
+  with open(f"schedule/{file_name}.py", "w") as f:
+    f.write("import os; os.remove(__file__)")
+except FileNotFoundError:
+  import os
+  os.mkdir("schedule")
+  with open(f"schedule/{file_name}.py", "w") as f:
+    f.write("import os; os.remove(__file__)")
